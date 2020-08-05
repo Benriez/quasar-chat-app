b8613b8e0c1e172e4dcf0aa5450a6565869ecb7c
Benriez <33022072+Benriez@users.noreply.github.com>
2020-08-05 14:43:36 +0200

    to previous

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -6,19 +6,19 @@ var firebase = require("firebase/app");
 require("firebase/auth");
 require("firebase/database");
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyD1F9UrT9nOTesZ9RK07CrFhws7WIMjT5U",
-    authDomain: "quasar-chat-app-53ce0.firebaseapp.com",
-    databaseURL: "https://quasar-chat-app-53ce0.firebaseio.com",
-    projectId: "quasar-chat-app-53ce0",
-    storageBucket: "quasar-chat-app-53ce0.appspot.com",
-    messagingSenderId: "1079777141647",
-    appId: "1:1079777141647:web:d8b80d4278ce73b103e1fa"
-  };
-  // Initialize Firebase
-  let firebaseApp= firebase.initializeApp(firebaseConfig);
-  let firebaseAuth= firebaseApp.auth()
-  let firebaseDb= firebaseApp.database()
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD1F9UrT9nOTesZ9RK07CrFhws7WIMjT5U",
+  authDomain: "quasar-chat-app-53ce0.firebaseapp.com",
+  databaseURL: "https://quasar-chat-app-53ce0.firebaseio.com",
+  projectId: "quasar-chat-app-53ce0",
+  storageBucket: "quasar-chat-app-53ce0.appspot.com",
+  messagingSenderId: "1079777141647",
+  appId: "1:1079777141647:web:d8b80d4278ce73b103e1fa"
+};
+// Initialize Firebase
+let firebaseApp= firebase.initializeApp(firebaseConfig);
+let firebaseAuth= firebaseApp.auth()
+let firebaseDb= firebaseApp.database()
 
-  export {firebaseAuth, firebaseDb}
+export {firebaseAuth, firebaseDb}
