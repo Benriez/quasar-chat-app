c55b88b504a365cb49867bd876c60f65ecb9af65
Benriez <33022072+Benriez@users.noreply.github.com>
2020-08-05 14:25:40 +0200

    vuex configured

---
+++ src/store/store.js
@@ -0,0 +1,27 @@
+//all the data of the app will go here
+const state = {
+
+}
+// methods wich will manipulate the data
+// these methods cannot be asynch
+const mutations = {
+
+}
+// also methods but can be asynch
+// can trigger mutations
+const actions = {
+
+}
+// methods to grab data from the state and 
+// make that data available for vue components
+const getters = {
+
+}
+
+export default {
+    namespaced:true,
+    state,
+    mutations,
+    actions,
+    getters
+}
