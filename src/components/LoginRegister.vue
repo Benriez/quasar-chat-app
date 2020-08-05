bbdb42c053ea046048d70a3d2a3109b48fd1e42d
Benriez <33022072+Benriez@users.noreply.github.com>
2020-08-05 13:48:39 +0200

    added q-input and configured

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -1,10 +1,24 @@
 <template>
-    <p> {{tab}} </p>
+    <q-form>
+      <q-input
+        outlined
+        v-model="formData.name"
+        label="Name" /> 
+    </q-form>
 </template>
 
 <script>
 export default {
-    props: ['tab']
+    props: ['tab'],
+    data() {
+        return{
+            formData: {
+                name: '',
+                email: '',
+                password: ''
+            }
+        }
+    }
 }
 </script>
 
